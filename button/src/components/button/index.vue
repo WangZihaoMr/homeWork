75e7a9c1abd00010a3291f0608c376af42195566
WangZihao <2597160811@qq.com>
2022-07-18 21:29:19 +0800

    项目的基础配置

---
+++ button/src/components/button/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wang-button">王子豪</div>
+</template>
+
+<script>
+export default {
+  name: 'buttonView',
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+<style scoped lang="scss"></style>
