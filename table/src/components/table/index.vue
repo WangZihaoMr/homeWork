8f0a2dedc8169f967d1bb740acd4a61ab4f27630
WangZihao <2597160811@qq.com>
2022-07-19 20:08:01 +0800

    table的属性：index序号、selection复选框

--- table/src/components/table/index.vue
+++ table/src/components/table/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="table-wrapper">
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column v-if="index" type="index" label="序号" width="55">
+      </el-table-column>
+      <el-table-column v-if="selection" type="selection" width="55">
+      </el-table-column>
+      <el-table-column
+        v-for="(item, index) in cloumns"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label"
+        :width="item.width"
+      >
+      </el-table-column>
+      <!-- <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column> -->
     </el-table>
   </div>
 </template>
@@ -11,28 +22,39 @@
 <script>
 export default {
   name: 'tableView',
-  components: {},
+  props: {
+    cloumns: {
+      type: Array,
+      default: () => []
+    },
+    selection: Boolean,
+    index: Boolean
+  },
   data() {
     return {
       tableData: [
         {
           date: '2016-05-02',
           name: '王小虎',
+          gender: '男',
           address: '上海市普陀区金沙江路 1518 弄'
         },
         {
           date: '2016-05-04',
           name: '王小虎',
+          gender: '男',
           address: '上海市普陀区金沙江路 1517 弄'
         },
         {
           date: '2016-05-01',
           name: '王小虎',
+          gender: '男',
           address: '上海市普陀区金沙江路 1519 弄'
         },
         {
           date: '2016-05-03',
           name: '王小虎',
+          gender: '男',
           address: '上海市普陀区金沙江路 1516 弄'
         }
       ]
--- table/src/views/HomeView.vue
+++ table/src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
-    <wang-table></wang-table>
+    <wang-table :cloumns="cloumns" selection index></wang-table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      cloumns: [
+        { prop: 'date', label: '日期', width: '180' },
+        { prop: 'name', label: '姓名', width: '180' },
+        { prop: 'address', label: '地址', width: '200' },
+        { prop: 'gender', label: '性别', width: '80' }
+      ]
+    }
+  },
   components: {
     wangTable: () => import('../components/table')
   }
