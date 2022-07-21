fbfc684f107e18bf7af429c1bd46c64dc3a541bc
WangZihao <2597160811@qq.com>
2022-07-21 19:39:01 +0800

    表单的item && 表单的字段绑定

---
+++ form/src/components/form/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-container">
+    <el-form ref="form" :model="field" label-width="80px">
+      <el-form-item
+        v-for="item in itemArray"
+        :type="item.type"
+        :key="item.prop"
+        :label="item.label"
+      >
+        <el-input v-model="field[item.prop]"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'formView',
+  props: {
+    itemArray: {
+      type: Array,
+      default: () => []
+    },
+    field: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      form: {
+        name: ''
+      }
+    }
+  }
+}
+</script>
+<style scoped lang="scss"></style>
---
+++ form/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <wang-form :itemArray="itemArray" :field="formField"></wang-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      itemArray: [
+        {
+          label: '姓名',
+          type: 'input',
+          prop: 'name'
+        },
+        {
+          label: '密码',
+          type: 'input',
+          prop: 'password'
+        }
+      ],
+      formField: {
+        name: '王炸',
+        password: '123456'
+      }
+    }
+  },
+  components: {
+    wangForm: () => import('../components/form')
+  }
+}
+</script>
